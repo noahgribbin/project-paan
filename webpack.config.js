9494ed0fefcbd88c1b105a421580c3fdc5dc823e
noahgribbin <noahgribbin@yahoo.com>
2017-06-01 11:45:26 -0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const dotenv = require('dotenv');
+const webpack = require('webpack');
+// const HTMLPlugin = require('html-webpack-plugin');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+dotenv.load();
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+};
