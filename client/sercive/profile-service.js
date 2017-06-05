6dc1f5158c0176f24a6c291aaf9e50f96ecca62f
noahgribbin <noahgribbin@yahoo.com>
2017-06-05 14:34:15 -0700

    finished profile functions

--- client/sercive/profile-service.js
+++ client/sercive/profile-service.js
@@ -25,7 +25,7 @@ function profileService($q, $log, $http, authService) {
       return $http.post(url, profile, config);
     })
     .then( res => {
-      $log.log('profile created');
+      $log.log('profile created.');
       let profile = res.data;
       service.profiles.unshift(profile);
       return profile;
@@ -39,21 +39,76 @@ function profileService($q, $log, $http, authService) {
   service.fetchProfile = function(profileID) {
     $log.debug('profileService.fetchProfile');
 
-    let url =  
+    let url = `${__API_URL__}/api/profile/${profileID}`;
+    let config = {
+      headers: {
+        Accept: 'application/json'
+      }
+    };
+
+    return $http.get(url, config)
+    .then( res => {
+      $log.log('Profile retrieved.', res);
+      service.profile = res.data;
+      return service.profile;
+    })
+    .catch( err => {
+      $log.error(err.message);
+      return $q.reject(err);
+    });
   };
 
+  service.editProfile = function(profileID, profileData) {
+    $log.debug('profileService.editProfile');
 
+    return authService.getToken()
+    .then( token => {
+      let url = `${__API_URL__}/api/profile/${profileID}`;
+      let config = {
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        }
+      };
 
+      return $http.put(url, profileData, config);
+    })
+    .then( res => {
+      $log.log('Profile sucessfully edited.');
+      service.profile = res.data;
+      return service.profile;
+    })
+    .catch( err => {
+      $log.error( err.message);
+      return $q.reject(err);
+    });
+  };
 
+  service.deleteProfile = function(profileID) {
+    $log.debug('profileService.deleteProfile');
 
+    return authService.getToken()
+    .then( token => {
+      let url = `${__API_URL__}/api/profile/${profileID}`;
+      let config = {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      };
 
+      return $http.delete(url, config);
+    })
+    .then( res => {
+      $log.log('Profile deleted.');
+      service.profile = res.data;
+      return service.profile;
+    })
+    .catch( err => {
+      $log.error(err.message);
+      return $q.reject(err);
+    });
+  };
 
-
-
-
-
-
-
-
-
+  return service;
 }
